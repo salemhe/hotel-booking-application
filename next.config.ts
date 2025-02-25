20d35f525c710bd37a8c7d561e69250111383c41
Wisdom Ofogba <wisetega007@gmail.com>
2025-02-25 22:20:04 +0100

    updated images config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ['https://hotel-booking-app-backend-30q1.onrender.com'],
+  },
 };
 
 export default nextConfig;
