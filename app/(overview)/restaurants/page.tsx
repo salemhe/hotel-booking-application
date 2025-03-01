d8b7ff5eab4d8d705d099bc59aa2d60792955ce0
Wisdom Ofogba <wisetega007@gmail.com>
2025-03-01 23:47:17 +0100

    testing

--- app/(overview)/restaurants/page.tsx
+++ app/(overview)/restaurants/page.tsx
@@ -1,12 +1,11 @@
-import Loading from "@/components/loading"
-import Restaurants from "@/components/Restaurants"
-import { Suspense } from "react"
+import Loading from "@/components/loading";
+import Restaurants from "@/components/Restaurants";
+import { Suspense } from "react";
 
 export default function RestaurantsPage() {
   return (
     <Suspense fallback={<Loading />}>
       <Restaurants />
     </Suspense>
-  )
+  );
 }
-
