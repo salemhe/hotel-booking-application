8711803f4f08010c4b9cafb9ecda33dacc9d3de6
Godstime Samuel <godstimesml@gmail.com>
2025-08-02 11:47:06 -0700

    made all fetches go live both on user faces and vendor faces

--- app/vendor-dashboard/restaurant/layout.tsx
+++ app/vendor-dashboard/restaurant/layout.tsx
@@ -2,4 +2,4 @@ import SidebarShell from "./SidebarShell";
 
 export default function RestaurantLayout({ children }: { children: React.ReactNode }) {
   return <SidebarShell>{children}</SidebarShell>;
-}
+} 
